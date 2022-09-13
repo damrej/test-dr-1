5da377213ef99e6664f3d65d6ab7e1d6c9f429ff
Damian Rejczak <damian.rejczak@gmail.com>
2022-09-13 20:48:02 +0200

    initial commit

---
+++ src/test/java/com/example/testdr/TestDrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
